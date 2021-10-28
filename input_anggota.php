8b964ad57d73939a8c1c5c5c9a595844610862d4
dedefirman2002 <im5837952@gmail.com>
2021-10-28 21:05:40 +0700

    contoh input

---
+++ input_anggota.php
@@ -0,0 +1,36 @@
+<?php
+include("koneksi.php");
+
+if(isset($_POST['save'])){
+$query_input=mysqli_query($koneksi,"insert into anggota(nama,alamat,no_tlp)
+values(
+'".$_POST['nama']."',
+'".$_POST['alamat']."',
+'".$_POST['no_tlp']."')");
+
+if($query_input){
+header('location:view_anggota.php');
+}else{
+	echo mysqli_error();
+}
+}
+?>
+<form method="POST">
+<table class="table table-bordered" border="1">
+	<tr>
+		<td>Nama</td>
+		<td><input type="text" name="nama" class="form-control"></td>
+	</tr>
+	<tr>
+		<td>Alamat</td>
+		<td><input type="text" name="alamat" class="form-control"></td>
+	</tr>
+	<tr>
+		<td>No Tlpn</td>
+		<td><input type="text" name="no_tlp" class="form-control"></td>
+	</tr>
+		<tr>
+			<td><input type="submit" name="save" class="btn btn-danger"></td>
+		</tr>
+</table>
+</form>
