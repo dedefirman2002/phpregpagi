51fe8af3cce7e56a43eed82d9e768ebcb5bd8169
dedefirman2002 <im5837952@gmail.com>
2021-11-16 13:16:45 +0700

    Login Akun

--- view_kategori.php
+++ view_kategori.php
@@ -1,12 +1,24 @@
 <?php 
 include("koneksi.php");
+session_start();
+if(!($_SESSION['username'])){
+		echo "<SCRIPT>alert('silahkan login terlebih dahulu');window.location='index.php'</SCRIPT>";
+}
+
 $query_view=mysqli_query($koneksi,"select * from kategori");
 
 ?>
+</br>
+<?php
+if( $_SESSION['level']== "admin"){
+	echo '<a href="input_kategori.php" class="btn btn danger">Tambah Kategori</a>';
+}?> <a href="home.php">Halaman Utama</a>
 <table class="table table-bordered" border="1">
 	<tr>
 		<td>Id Kategori</td>
 		<td>Nama</td>
+		<td colspan="4">Action</td>
+		<?php if($_SESSION['level'=="admin"])?>
 	</tr>
 <?php 
 $id_kategori=1;
@@ -14,6 +26,8 @@ while($tampil=mysqli_fetch_array($query_view)){?>
 	<tr>
 		<td><?php echo $id_kategori++;?></td>
 		<td><?php echo $tampil['nama'];?></td>
+		<?php if($_SESSION['level']=="admin"){echo '<td><a href="edit_kategori.php?id_kategori='.$tampil ['id_kategori'].'">Edit</a></td>';
+		echo'<td><a href="hapus_kategori.php?id_kategori='.$tampil ['id_kategori'].'" onclick="return confirm("yakin keluar?")">hapus</a></td>';}?>
 	</tr>
 <?php }?>
 
