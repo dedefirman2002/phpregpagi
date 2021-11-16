b244309872013a6aef54ebc883b82b49c2fb9abf
dedefirman2002 <im5837952@gmail.com>
2021-11-16 13:11:51 +0700

    Login Akun

--- view_anggota.php
+++ view_anggota.php
@@ -1,14 +1,25 @@
 <?php 
 include("koneksi.php");
-$query_view=mysqli_query($koneksi,"select * from anggota");
+session_start();
+if(!($_SESSION['username'])){
+		echo "<SCRIPT>alert('silahkan login terlebih dahulu');window.location='index.php'</SCRIPT>";
+}
 
+$query_view=mysqli_query($koneksi,"select * from anggota");
 ?>
+</br>
+<?php
+if( $_SESSION['level']== "admin"){
+	echo '<a href="input_anggota.php" class="btn btn danger">Tambah Anggota</a>';
+}?> <a href="home.php">Halaman Utama</a>
 <table class="table table-bordered" border="1">
 	<tr>
 		<td>No</td>
 		<td>Nama</td>
 		<td>Alamat</td>
 		<td>No Tlpn</td>
+		<td colspan="4">Action</td>
+		<?php if($_SESSION['level'=="admin"])?>
 	</tr>
 <?php 
 $no=1;
@@ -18,7 +29,10 @@ while($tampil=mysqli_fetch_array($query_view)){?>
 		<td><?php echo $tampil['nama'];?></td>
 		<td><?php echo $tampil['alamat'];?></td>
 		<td><?php echo $tampil['no_tlp'];?></td>
+		<?php if($_SESSION['level']=="admin"){ echo '<td><a href="edit_anggota.php?id_anggota='.$tampil ['id_anggota'].'">Edit</a></td>';
+		 echo'<td><a href="hapus_anggota.php?id_anggota='.$tampil ['id_anggota'].'" onclick="return confirm("yakin keluar?")">hapus</a></td>';}?>
 	</tr>
 <?php }?>
+
 
 </table>
