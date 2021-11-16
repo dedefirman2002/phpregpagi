0d49881fc09dde48baa913d4d0c57b7bb15fbb2e
dedefirman2002 <im5837952@gmail.com>
2021-11-16 17:22:38 +0700

    Login Akun

--- view_anggota.php
+++ view_anggota.php
@@ -19,7 +19,7 @@ if( $_SESSION['level']== "admin"){
 		<td>Alamat</td>
 		<td>No Tlpn</td>
 		<td colspan="4">Action</td>
-		<?php if($_SESSION['level'=="admin"])?>
+		<?php if($_SESSION['level']=="admin")?>
 	</tr>
 <?php 
 $no=1;
